5ec3785bc75114fd6559d81570a83c7043c2b2ed
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-11-22 03:11:18 +0600

    init

--- app/config/asgi.py
+++ app/config/asgi.py
@@ -14,10 +14,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from rag.routing import websocket_urlpatterns
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+from rag.routing import websocket_urlpatterns
 
 django_asgi_app = get_asgi_application()
 
