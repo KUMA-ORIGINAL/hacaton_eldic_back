e14a3c35cc8b9fa5ef4cc927e7682c0b4ed77d22
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-11-22 09:26:42 +0600

    init

--- app/config/asgi.py
+++ app/config/asgi.py
@@ -22,11 +22,11 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                websocket_urlpatterns
-            )
+    "websocket":
+
+        URLRouter(
+            websocket_urlpatterns
         )
-    ),
+
+
 })
